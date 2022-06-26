dc52b0d4ae4cbd8cd9b5b0eb2f2bba9a8c7bd8d5
Mark <obrishtigoogl@gmail.com>
2022-06-26 11:16:56 +0300

    Result01

--- main/exel_in_pandas.py
+++ main/exel_in_pandas.py
@@ -7,21 +7,20 @@ def name_table():
     files = os.listdir(dirname)
     last_elements = max(files)
     element1, element2 = last_elements.split(sep='.')
-    element1 = int(element1) + 1
-    name0 = str(element1) + '.' + element2
+    name0 = str(int(element1) + 1) + '.' + element2
     name1 = last_elements
-    return [name0, name1]
+    name3 = element1 + '.txt'
+    return [name0, name1, name3]
 
 
 def conver_xlsx():
-    new_name = name_table()[1].split(sep='.')[0]
-    data = pd.read_excel(r'test/' + name_table()[1])
-    itog = data.to_html(r'result/' + new_name + '.txt', index=None, encoding='utf-8', na_rep='')
-    dirname = r'result/'
-    files = os.listdir(dirname)
-    last_elements = max(files)
-    f = open(new_name + '.txt', 'r', encoding="utf-8")
-    gg = f.read()
+    try:
+        data = pd.read_excel(r'test/' + name_table()[1])
+        itog = data.to_html(r'result/' + name_table()[2], index=None, encoding='utf-8', na_rep='')
+        f = open(r'result/' + name_table()[2], 'r', encoding="utf-8")
+        gg = f.read()
+    except Exception:
+        gg = "С Вашим документом что-то не так"
     return gg
 
 if __name__ == '__main__':
--- main/telegram.py
+++ main/telegram.py
@@ -13,12 +13,16 @@ def start(message):
 
 @bot.message_handler(content_types=['document'])
 def xlsx(message):
-    file_info = bot.get_file(message.document.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    src = r'test/' + name_table()[0];
-    with open(src, 'wb') as new_file:
-        new_file.write(downloaded_file)
-    bot.send_message(message.chat.id, conver_xlsx())
+    try:
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        src = r'test/' + name_table()[0];
+        with open(src, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        bot.send_message(message.chat.id, conver_xlsx())
+    except Exception:
+        bot.send_message(message.chat.id, 'Что-то пошло не так')
+
 
 
 bot.polling(none_stop=True)
